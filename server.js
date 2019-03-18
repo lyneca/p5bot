bb8194ac01dcc660b40a4a8966290011ee99f941
Luke Tuthill <lukemtuthill@gmail.com>
2019-03-18 14:34:11 +1100

    Remove debug print

--- server.js
+++ server.js
@@ -77,7 +77,6 @@ function getThreadParent(thread_ts, channel) {
             }
         }
     ).then(request => {
-        console.log(request.data)
         return request.data.messages[0].files[0].url_private;
     });
 }
